8558a02cde9458bed921099b10c57e93b45c0eb1
debasishdasagt <debasishdasagt@users.noreply.github.com>
2019-05-20 07:48:37 +0530

    zxz

--- config.php
+++ config.php
@@ -5,16 +5,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-<<<<<<< HEAD
+
 $db_server="localhost";
 $db_name="biht_elearning";
 $db_username="root";
 $db_password="devdas";
 $conn=  mysql_connect($db_server,$db_username,$db_password);
 $db= mysql_select_db($db_name);
-=======
-/* Let me test this  sdsdsdsS*/
-$conn=  mysql_connect('localhost','root','devdas');
-$db= mysql_select_db('biht_elearning');
->>>>>>> 9354ad90cd2967fa38fde31817690a28b22f0e90
+
 ?>
